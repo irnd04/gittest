e24ce499555f9cfb64258da3551621943cc4136f
이재국 <jaeguk.lee@nhnservice.com>
2020-04-24 10:35:50 +0900

    test

---
+++ src/main/java/kr/co/mayfarm/service/GitTestService.java
@@ -0,0 +1,14 @@
+package kr.co.mayfarm.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class GitTestService {
+
+    public void test() {
+        log.info("git.. test...");
+    }
+}
